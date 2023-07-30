03709e1ae299f20e7fcc04fe13560e8e0f6f4c8f
PabaleloLucia <48206169+PabaleloLucia@users.noreply.github.com>
2023-07-30 13:54:52 +0200

    Added the GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">hello Botswana</div>;
+  return (
+    <div className="App">
+      hello Botswana
+      <br />
+      <small>
+        This project was coded by Pabalelo Tsebempe and is open-sourced on {""}
+        <a
+          href="https://github.com/PabaleloLucia/react-weather-app"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </small>
+    </div>
+  );
 }
