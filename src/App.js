7bb9c47f1d3977f100586c1c0138f3849606e952
PabaleloLucia <48206169+PabaleloLucia@users.noreply.github.com>
2023-08-01 18:51:56 +0200

    Added basic styling and created links for the footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Weather from "./Weather";
 import "./App.css";
 
+
 export default function App() {
   return (
     <div className="App">
@@ -9,7 +10,13 @@ export default function App() {
         <Weather />
 
         <small>
-          This project was coded by Pabalelo Tsebempe and is open-sourced on{" "}
+          This project was coded by <a
+            href="mailto:pabalelotsebempe@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+       Pabalelo Tsebempe
+          </a> and is open-sourced on{" "}
           {""}
           <a
             href="https://github.com/PabaleloLucia/react-weather-app"
@@ -17,6 +24,12 @@ export default function App() {
             rel="noreferrer"
           >
             GitHub
+          </a> and hosted on  <a
+            href="https://github.com/PabaleloLucia/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
           </a>
         </small>
       </div>
--- src/Weather.js
+++ src/Weather.js
@@ -6,21 +6,22 @@ export default function Weather() {
     <div className="weather">
       <form>
         <div className="row">
-          <div className="col-6">
+          <div className="col-8">
             <input
               className="form-control"
               type="search"
               placeholder="type city name..."
+              autoFocus="on"
             />
           </div>
-          <div className="col-3">
+          <div className="col-2">
             <input type="submit" value="Search" className="btn btn-secondary" />
           </div>
-          <div className="col-3">
+          <div className="col-2">
             <input
               type="submit"
               value="Current"
-              className="btn btn-secondary"
+              className="btn btn-secondary "
             />
           </div>
         </div>
@@ -30,19 +31,18 @@ export default function Weather() {
         <li>Wednesday 11:00</li>
         <li>Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Mostly Cloudy"
           />
-
           <strong>16</strong>
-          <span>
+          <span className="units">
             <a href="./"> ℃</a> | <a href="./">℉</a>
           </span>
         </div>
-        <div className="col-6">
+        <div className="col-6 mt-3">
           <ul>
             <li>Humidity: 60%</li>
             <li>Wind: 2km/h</li>
