b0e308fa25aa92d287285d4ff77a056cf874e220
PabaleloLucia <48206169+PabaleloLucia@users.noreply.github.com>
2023-08-03 18:12:03 +0200

    Installed axios ,integrated API and well as adding the react-loader-spinner.

--- src/App.js
+++ src/App.js
@@ -2,29 +2,31 @@ import React from "react";
 import Weather from "./Weather";
 import "./App.css";
 
-
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Gaborone" />
 
         <small>
-          This project was coded by <a
+          This project was coded by{" "}
+          <a
             href="mailto:pabalelotsebempe@gmail.com"
             target="_blank"
             rel="noreferrer"
           >
-       Pabalelo Tsebempe
-          </a> and is open-sourced on{" "}
-          {""}
+            Pabalelo Tsebempe
+          </a>{" "}
+          and is open-sourced on {""}
           <a
             href="https://github.com/PabaleloLucia/react-weather-app"
             target="_blank"
             rel="noreferrer"
           >
             GitHub
-          </a> and hosted on  <a
+          </a>{" "}
+          and hosted on{" "}
+          <a
             href="https://remarkable-longma-d613fa.netlify.app/"
             target="_blank"
             rel="noreferrer"
--- src/Weather.js
+++ src/Weather.js
@@ -1,54 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import { InfinitySpin } from "react-loader-spinner";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="weather">
-      <form>
-        <div className="row">
-          <div className="col-8">
-            <input
-              className="form-control"
-              type="search"
-              placeholder="type city name..."
-              autoFocus="on"
-            />
+export default function Weather(props) {
+  const [weatherInfo, setWeatherInfo] = useState({ ready: false });
+
+  function handleResponse(response) {
+    setWeatherInfo({
+      ready: true,
+      temperature: response.data.temperature.current,
+      city: response.data.city,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.condition.description,
+      iconUrl:
+        "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png",
+      dateTime: "Wednesday 11:00",
+    });
+  }
+
+  if (weatherInfo.ready) {
+    return (
+      <div className="weather">
+        <form>
+          <div className="row">
+            <div className="col-8">
+              <input
+                className="form-control"
+                type="search"
+                placeholder="type city name..."
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-2">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-secondary"
+              />
+            </div>
+            <div className="col-2">
+              <input
+                type="submit"
+                value="Current"
+                className="btn btn-secondary "
+              />
+            </div>
           </div>
-          <div className="col-2">
-            <input type="submit" value="Search" className="btn btn-secondary" />
+        </form>
+        <h1>{weatherInfo.city}</h1>
+        <ul>
+          <li>{weatherInfo.dateTime}</li>
+          <li className="text-capitalize">{weatherInfo.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <img src={weatherInfo.iconUrl} alt={weatherInfo.description} />
+            <strong>{Math.round(weatherInfo.temperature)}</strong>
+            <span className="units">
+              <a href="./"> ℃</a> | <a href="./">℉</a>
+            </span>
           </div>
-          <div className="col-2">
-            <input
-              type="submit"
-              value="Current"
-              className="btn btn-secondary "
-            />
+          <div className="col-6 mt-3">
+            <ul>
+              <li>Humidity: {weatherInfo.humidity}%</li>
+              <li>Wind: {Math.round(weatherInfo.wind)}km/h</li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <h1>Gaborone</h1>
-      <ul>
-        <li>Wednesday 11:00</li>
-        <li>Cloudy</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Cloudy"
-          />
-          <strong>16</strong>
-          <span className="units">
-            <a href="./"> ℃</a> | <a href="./">℉</a>
-          </span>
-        </div>
-        <div className="col-6 mt-3">
-          <ul>
-            <li>Humidity: 60%</li>
-            <li>Wind: 2km/h</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "b44b3b21bbcdc2007ba9f404ao7et12a";
+
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return <InfinitySpin width="200" color="#4fa94d" />;
+  }
 }
