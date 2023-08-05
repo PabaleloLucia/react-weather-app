89dbdae2d71c1cf4479e5591acdcc1cfc6b02e19
PabaleloLucia <48206169+PabaleloLucia@users.noreply.github.com>
2023-08-05 15:57:59 +0200

    replaced the static icon with the dynamic URL and interpolated  the API  response

--- src/Weather.js
+++ src/Weather.js
@@ -17,8 +17,7 @@ export default function Weather(props) {
       humidity: response.data.temperature.humidity,
       wind: response.data.wind.speed,
       description: response.data.condition.description,
-      iconUrl:
-        "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png",
+      iconUrl: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
       date: new Date(response.data.time * 1000),
       temperature: response.data.temperature.current,
     });
